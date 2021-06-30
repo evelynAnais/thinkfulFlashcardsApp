53898ebb7089c224a3caae08ef1820d16c8e3859
evelynAnais <evelyn.a.n.d.30@gmail.com>
2021-06-30 10:55:21 -0600

    updated with code that qualified likes in deck

--- src/Layout/Components/Deck.js
+++ src/Layout/Components/Deck.js
@@ -26,6 +26,36 @@ function Deck({ deck }) {
       }
       <button className='btn btn-danger'><FontAwesomeIcon icon={faTrash}/></button>
     </div>
+
+
+  // Code that qualified likes    
+  // switch(url) {
+  //   case '/':
+  //     return (
+  //       <div className='container boarder boarder-secondary p-2 mt-2'>
+  //         <h2>{deck.name}</h2>
+  //         <p>{deck.cards?.length} cards</p>
+  //         <p>{deck.description}</p>
+  //         <Link to={`/decks/${deck.id}`} className='btn btn-secondary'>View</Link>
+  //         <Link to={`/decks/${deck.id}/study`} className='btn btn-primary'>Study</Link>
+  //         <button className='btn btn-danger'><FontAwesomeIcon icon={faTrash} /></button>
+  //       </div>
+  //     );
+  //     break;
+  //   default:
+  //     return (
+  //       <div className='container boarder boarder-secondary p-2 mt-2'>
+  //         <h2>{deck.name}</h2>
+  //         <p>{deck.description}</p>
+  //         <Link to={`/decks/${deck.id}/edit`} className='btn btn-secondary'>Edit</Link>
+  //         <Link to={`/decks/${deck.id}/study`} className='btn btn-primary'>Study</Link>
+  //         <Link to={`/decks/${deck.id}/cards/new`} className='btn btn-primary'>Add Card</Link>
+  //         <button className='btn btn-danger'><FontAwesomeIcon icon={faTrash} /></button>
+  //       </div>
+  //     );
+  // }
+  // }
+
   );
 }
 
